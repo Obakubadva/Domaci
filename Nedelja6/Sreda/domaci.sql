e45dbe754ae5ec0faead1fac55c519da6f63e021
Obakubadva <88008672+Obakubadva@users.noreply.github.com>
2021-09-07 21:35:04 +0200

    Update domaci.sql

--- Nedelja6/Sreda/domaci.sql
+++ Nedelja6/Sreda/domaci.sql
@@ -1,1 +1,5 @@
-
+SELECT * FROM profesori Where godine > 45;
+SELECT ime, prezime, ucenikid FROM ucenici WHERE godine > 15 AND prosek > 6.5;
+SELECT godine FROM ucenici ORDER BY godine DESC;
+SELECT * FROM profesori WHERE predmet = 'Informatika';
+SELECT ucenikid, ime, prezime FROM ucenici WHERE LENGTH(ime) = 6;

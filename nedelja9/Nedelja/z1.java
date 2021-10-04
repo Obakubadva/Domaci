45137fda39ba488a6ca583a0199c6cc2bc1ae030
Obakubadva <88008672+Obakubadva@users.noreply.github.com>
2021-10-04 13:40:03 +0200

    Create z1.java

---
+++ nedelja9/Nedelja/z1.java
@@ -0,0 +1,42 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+public class fajl {
+    public static void main(String[] args) {
+        try{
+            BufferedWriter bw= new BufferedWriter(new FileWriter("Text.txt"));
+            Random rand =new Random();
+            int a=0;
+            while ((a<1000)) {
+                int pickedNumber = rand.nextInt(14) + 1;
+                int[] niz = new int[pickedNumber];
+                for (int i = 0; i < niz.length; i++) {
+                    int numberArray = rand.nextInt(10);
+                    niz[i] = numberArray;
+                }
+                String s = Arrays.toString(niz);
+                s=s.substring(1, s.length()-1);
+                int[] n = new int[niz.length];
+                int j = 0;
+                for (int i = 0; i < niz.length; i++) {
+                    if (niz[i] != 0) {
+                        n[j] = niz[i];
+                        j++;
+                    }
+                }
+                    String b = Arrays.toString(n);
+                    b=b.substring(1, b.length() - 1);
+                bw.append("assertEquals(new int[]{"+b+"}, imeFunkcije(new int[]{"+s+"});");
+                bw.newLine();
+                    a++;
+                }
+            bw.close();
+            }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}

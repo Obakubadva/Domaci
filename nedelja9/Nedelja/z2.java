9a580a26f1a11685bd69c1095a74eaa91211ff89
Obakubadva <88008672+Obakubadva@users.noreply.github.com>
2021-10-05 14:46:29 +0200

    Update z2.java

--- nedelja9/Nedelja/z2.java
+++ nedelja9/Nedelja/z2.java
@@ -1,17 +1,17 @@
 public class Cesar {
     public static String decr(String str, int a){
-        String decText="";
+        String decText = "";
         for (int i = 0; i < str.length(); i++) {
-            char ch=str.charAt(i);
-            char c= (char)(ch+a);
-            decText=decText.concat(String.valueOf(c));
+            char ch = str.charAt(i);
+            char c = (char)(ch+a);
+            decText = decText.concat(String.valueOf(c));
         }
 
         return decText;
     }
     public static void main(String[] args) {
-        String text=">]aOZXWm[Wm\\OmaZOQYm^`WdOb\\cm^]`cYc(m8OdO\":gTS";
-        int number=18;
+        String text = ">]aOZXWm[Wm\\OmaZOQYm^`WdOb\\cm^]`cYc(m8OdO\":gTS";
+        int number = 18;
         System.out.println(decr(text, number));
     }
 }
